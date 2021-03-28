786f4a8938804104ebaad932531301c2d80d9cae
Alexandr <sanjok@bk.ru>
2021-03-28 22:13:24 +0300

    Lesson 1

---
+++ PosadskovLesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PosadskovLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет,  {name}. Сегодня {DateTime.Now.ToShortDateString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
