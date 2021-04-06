832405c4f52b14ff997d6dc0299dfa11a8cac82a
Alexandr <sanjok@bk.ru>
2021-04-06 23:38:23 +0300

    Lesson 3

--- PosadskovLesson3/Program.cs
+++ PosadskovLesson3/Program.cs
@@ -12,15 +12,29 @@ namespace PosadskovLesson3
 
             //InvertedWord();
 
-            SeeBatle();
+            //SeeBatle();
+
+            int[] arr = new int[7];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            ShiftArray<int>(arr, -4);
+
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
 
             Console.ReadKey();
         }
-
+        #region
         public static void PrintMassivDiagonal()
         {
-            int[][] mas = new int[][] { 
-                                        new int[]{ 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5 }, new int[]{ 1, 2, 3, 4, 5 }, 
+            int[][] mas = new int[][] {
+                                        new int[]{ 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5 }, new int[]{ 1, 2, 3, 4, 5 },
                                         new int[]{ 1, 2, 3, 4, 5 }, new int[]{ 1, 2, 3, 4, 5 } };
 
             Console.WriteLine("Исходный массив\n");
@@ -51,26 +65,26 @@ namespace PosadskovLesson3
             }
         }
 
-        public static void Phonebook ()
+        public static void Phonebook()
         {
-            string[,] phonebook = new string[5, 2] { {"Дима", "111-11-11/email@mail.ru" }, 
-                                                    { "Андрей", "222-22-22/email@mail.ru" }, 
+            string[,] phonebook = new string[5, 2] { {"Дима", "111-11-11/email@mail.ru" },
+                                                    { "Андрей", "222-22-22/email@mail.ru" },
                                                     { "Игорь", "333-33-33/email@mail.ru" },
                                                     { "Юля", "444-44-44/email@mail.ru" },
                                                     { "Барсик", "555-55-55/email@mail.ru" }, };
 
-            for (int i = 0; i < phonebook.Length/2; i++)
+            for (int i = 0; i < phonebook.Length / 2; i++)
             {
                 for (int j = 0; j < 2; j++)
-                {                    
+                {
                     Console.Write(phonebook[i, j]);
                     if (j != 1) Console.Write(" - ");
-                }                
+                }
                 Console.WriteLine('\n'); ;
             }
         }
 
-        public static void InvertedWord ()
+        public static void InvertedWord()
         {
             Console.WriteLine("Введите слово\n");
             string s = Console.ReadLine();
@@ -82,7 +96,7 @@ namespace PosadskovLesson3
             }
         }
 
-        public static void SeeBatle ()
+        public static void SeeBatle()
         {
             char[,] seeMap = new char[10, 10]
             {
@@ -98,65 +112,115 @@ namespace PosadskovLesson3
                 { 'O','O','O','O','O','O','X','X','O','O'}
             };
 
-            for (int i = 0; i < seeMap.Length/10; i++)
+            for (int i = 0; i < seeMap.Length / 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
                     Console.Write(seeMap[i, j] + " ");
                 }
-                Console.WriteLine(); 
+                Console.WriteLine();
             }
             Console.WriteLine(); ;
         }
 
-
-        public static void  RotateMassiv<T> (T[] massiv, int rotate)
-        {            
-            if (massiv.Length <= 1)
+        public static void RotateMassiv<T>(T[] array, int rotate)
+        {
+            if (array.Length <= 1)
             {
                 Console.WriteLine("Массив пустой или слишком короткий.\nМассив должен сожерьжать больше одного элемента.");
                 return;
             }
 
             //отрезаем лишнее, если надо вращать на большее число, чем длина массива
-            if ((rotate %= massiv.Length) == 0) 
+            if ((rotate %= array.Length) == 0)
             {
                 return;
             }
 
-            T[] copyMassive = (T[]) massiv.Clone();
+            T[] copyMassive = (T[])array.Clone();
             copyMassive.Clone();
 
             if (rotate > 0)
             {
                 int i = rotate, j = 0;
-                for (; i < massiv.Length; i++, j++)
+                for (; i < array.Length; i++, j++)
                 {
-                    massiv[j] = copyMassive[i];
+                    array[j] = copyMassive[i];
                 }
 
                 i = 0;
                 for (; i < rotate; i++, j++)
                 {
-                    massiv[j] = copyMassive[i];
+                    array[j] = copyMassive[i];
                 }
             }
             else
             {
-                int i = massiv.Length + rotate, j = 0;
-                for (; i < massiv.Length; i++, j++)
+                int i = array.Length + rotate, j = 0;
+                for (; i < array.Length; i++, j++)
                 {
-                    massiv[j] = copyMassive[i];
+                    array[j] = copyMassive[i];
                 }
 
                 i = 0;
-                for (; i < massiv.Length + rotate; i++, j++)
+                for (; i < array.Length + rotate; i++, j++)
                 {
-                    massiv[j] = copyMassive[i];
+                    array[j] = copyMassive[i];
                 }
             }
-            
 
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam Тип переменных в массиве="T"></typeparam>
+        /// <param Массив="array"></param>
+        /// <param Величина сдвига строго в целых числах="shift"></param>
+        public static void ShiftArray<T>(T[] array, int shift)
+        {
+            if (array.Length <= 1)
+            {
+                Console.WriteLine("Массив пустой или слишком короткий.\nМассив должен содержать больше одного элемента.");
+                return;
+            }
+
+            //отрезаем лишнее, если надо вращать на большее число, чем длина массива
+            //и если сдвиг кратен длине массива, то сдвигать не надо
+            if ((shift %= array.Length) == 0)
+            {
+                return;
+            }
+
+            T temp = default;
+            if (shift > 0)
+            {
+                for (int i = 0; i < array.Length - 1; ++i)
+                {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+
+                ShiftArray<T>(array, --shift);
+            }
+            else
+            {
+                for (int i = 0; i < array.Length - 1; ++i)
+                {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+                ShiftArray<T>(array, --shift); // это не баг, а фича ))))))  за счет деления на остаток стремимся к условию ==0 и выходим из цикла
+            }
         }
     }
 }
+
+        #endregion
+
+        
+
+        
