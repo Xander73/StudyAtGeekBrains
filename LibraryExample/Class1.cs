10b2b13cd3d9b9af7dde0d8b1ca8b74e3857ca2d
Alexandr <sanjok@bk.ru>
2021-04-24 17:07:07 +0300

    Lesson8

---
+++ LibraryExample/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryExample
+{
+    public class Class1
+    {
+        public static void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
